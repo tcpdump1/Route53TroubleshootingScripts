1e771784a895bbd40e75ed6f740b18b6aaeda608
tcpdump1 <oruntachukwuemeka@gmail.com>
2021-08-23 13:26:47 +0000

    refactoring

--- src/weighted.py
+++ src/weighted.py
@@ -12,13 +12,18 @@ from re import search
 import collections
 import time
 
-RESULT_LIST.append(f'This test was carried out 10 times')
 
-def _collection_of_resolution(domain_name):
+def _collection_of_resolution(domain_name, write, Route53):
+    
     retrieve_auth_name_server_ip = search(WEIGHTED_REGEX, Route53Base.run_check_output(['dig', domain_name, '+trace', '+short']))
     retrieve_auth_name_server = Route53Base.run_check_output(['dig','-x', retrieve_auth_name_server_ip.group(2), '+short'])
-    RESULT_LIST.append(f'This test was done using {retrieve_auth_name_server} authoritative nameserver.\n') 
     
+    RESULT_LIST.append(f'This test was carried out 10 times\nThis test was done using {retrieve_auth_name_server} authoritative nameserver.\n')
+    
+    if write:
+        create_file = Route53.write_query_to_file('weighted.txt')
+        create_file.write(f'This test was carried out 10 times\nThis test was done using {retrieve_auth_name_server} authoritative nameserver.\n')
+        
     for i in range(10):
         a_record = Route53Base.run_check_output(['dig', domain_name, f'@{retrieve_auth_name_server_ip.group(2)}', '+short']).splitlines()
         LIST_OF_DNS_RESOLUTION.append(tuple(sorted(a_record)))
@@ -26,19 +31,33 @@ def _collection_of_resolution(domain_name):
         
     return LIST_OF_DNS_RESOLUTION
 
-def main():
-    args = Parser.args
-    dict_of_resolution=collections.Counter(_collection_of_resolution(args.domain))
 
+def _calculate_dns_ratio(dict_of_resolution, domain_name, write, Route53):
+    
     for dns_resolution, count_of_resolution in dict_of_resolution.items():
         percent = f'{round(((float(count_of_resolution)/10) * 100),2)}%'
-        RESULT_LIST.append(f'The DNS record {args.domain} resolved to {list(dns_resolution)} {count_of_resolution} times with {percent} ratio.')
+        RESULT_LIST.append(f'The DNS record {domain_name} resolved to {list(dns_resolution)} {count_of_resolution} times with {percent} ratio.')
         
-        if args.write:
+        if write:
             create_file = Route53.write_query_to_file('weighted.txt')
-            create_file.write(f'\nThe DNS record {args.domain} resolved {list(dns_resolution)} {count_of_resolution} times with {percent} ratio.')
-
+            create_file.write(f'\nThe DNS record {domain_name} resolved {list(dns_resolution)} {count_of_resolution} times with {percent} ratio.')
+    
+    create_file.write(f'\n')
     return("\n".join(RESULT_LIST))
+
+
+def main():
+    args = Parser.args
+    Route53 = Route53Base(args.domain)
+    dict_of_resolution = collections.Counter(_collection_of_resolution(args.domain, args.write, Route53))
+    
+    return _calculate_dns_ratio(
+        dict_of_resolution,
+        args.domain,
+        args.write,
+        Route53
+    )
+
 
 if __name__ == '__main__':
     print(main())
